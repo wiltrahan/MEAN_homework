f6365af0088280e7bb531da64bf5b11bbc089bf4
wiltrahan <twilorip@gmail.com>
2017-09-05 17:36:13 -0400

    cleaned up code

--- level_2/app.js
+++ level_2/app.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/public/index.html')
-// });
-
 app.use(express.static('public'));
 
 app.get('/cities', function(req, res) {
